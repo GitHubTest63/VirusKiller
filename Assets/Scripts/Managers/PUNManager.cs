df47754f22f2fda69916caf12d5a65a37961310c
COSTA Florent <costa_florent@hotmail.fr>
2015-04-22 00:01:07 +0200

    photon test

--- Assets/Scripts/Managers/PUNManager.cs
+++ Assets/Scripts/Managers/PUNManager.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class PUNManager : MonoBehaviour {
+public class PUNManager : MonoBehaviour
+{
+    private const string roomName = "Game";
+    private RoomInfo[] roomsList; public GameObject playerPrefab;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        }
+        else if (PhotonNetwork.room == null)
+        {
+            // Create Room
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                PhotonNetwork.CreateRoom(roomName + Guid.NewGuid().ToString("n"), true, true, 5);
+
+            // Join Room
+            if (roomsList != null)
+            {
+                for (int i = 0; i < roomsList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name))
+                        PhotonNetwork.JoinRoom(roomsList[i].name);
+                }
+            }
+        }
+    }
+
+    void OnReceivedRoomListUpdate()
+    {
+        roomsList = PhotonNetwork.GetRoomList();
+    }
+    void OnJoinedRoom()
+    {
+        // Spawn player
+        PhotonNetwork.Instantiate("Prefabs/" + playerPrefab.name, Vector3.up * 5, Quaternion.identity, 0);
+    }
 }
